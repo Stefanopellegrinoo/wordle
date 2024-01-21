26e8ffcbbcbd39b76254283baf4aa60e22859221
Stefanopellegrinoo <stefanopellegrino490@gmail.com>
2024-01-20 21:55:47 -0300

    u

--- backEnd/api/index.js
+++ backEnd/api/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3001;
 const { Client } = require("pg")
 const bodyParser = require('body-parser');
-const config = require('./config');
+const config = require('../config');
 
 
 const host = config.host;
